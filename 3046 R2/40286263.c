ee4ef8f633b07d8359f9e80b5fc5ef9b16e95087
LeeJU <LJU0912@outlook.com>
2022-03-19 03:26:38 +0900

    B5

---
+++ 3046 R2/40286263.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+    int a, b, c;
+
+    scanf("%d %d", &b, &a);
+
+    c = a * 2 - b;
+
+    printf("%d", c);
+
+
+    return 0;
+}
