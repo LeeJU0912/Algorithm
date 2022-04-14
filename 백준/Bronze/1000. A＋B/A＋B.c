ea27fc675cbd914c9967e54217d06fea5a615bec
LeeJU <101004585+LeeJU0912@users.noreply.github.com>
2022-04-14 13:35:30 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+    int a;
+    int b;
+
+    scanf("%d %d", &a, &b);
+    {
+        if (a > 0, b < 10) {
+            printf("%d", a + b);
+        }
+    }
+    return 0;
+}
