f2d0dceb5bf33bc3a051b7156f064764865b1537
LeeJU <101004585+LeeJU0912@users.noreply.github.com>
2022-04-27 07:29:15 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cmath>
+#include <ctime>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <string>
+#include <vector>
+#include <tuple>
+#include <functional>
+#include <map>
+
+using namespace std;
+
+priority_queue<int> q;
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	int a, b;
+	int i, g;
+
+	cin >> a;
+	cin.ignore();
+
+	for (i = 0; i < a; i++) {
+		cin >> b;
+		if (b == 0) {
+			if (q.size() == 0) {
+				cout << "0" << '\n';
+			}
+			else {
+				cout << q.top() << '\n';
+				q.pop();
+			}
+		}
+		else {
+			q.push(b);
+		}
+	}
+
+	return 0;
+}
