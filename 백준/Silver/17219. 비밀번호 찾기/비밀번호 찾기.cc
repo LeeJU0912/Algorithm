a5d22ff4168179a422826614135856af8a947152
LeeJU <101004585+LeeJU0912@users.noreply.github.com>
2022-05-05 01:07:56 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 236 ms, Memory: 16284 KB -BaekjoonHub

---
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cmath>
+#include <ctime>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <string>
+#include <vector>
+#include <tuple>
+#include <functional>
+#include <map>
+#include <set>
+#include <cstring>
+
+using namespace std;
+
+map<string, string> m;
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	int a;
+	int b;
+	int i;
+	string c, d;
+
+	cin >> a >> b;
+	for (i = 0; i < a; i++) {
+		cin >> c >> d;
+		m.insert(make_pair(c, d));
+	}
+	for (i = 0; i < b; i++) {
+		cin >> d;
+		map<string, string>::iterator iter = m.find(d);
+		cout << iter->second << '\n';
+	}
+	
+	return 0;
+}
