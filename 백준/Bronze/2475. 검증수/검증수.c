e0fee8cf64fb3e538709b2c0f1bf99fa72fcf42c
LeeJU <101004585+LeeJU0912@users.noreply.github.com>
2022-05-10 03:17:36 +0900

    [Bronze V] Title: 검증수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+    int a, b, c, d, e, f;
+
+    scanf("%d %d %d %d %d", &a, &b, &c, &d, &e);
+
+    f = (a * a + b * b + c * c + d * d + e * e) % 10;
+    printf("%d", f);
+
+    return 0;
+}
