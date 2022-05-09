afc54eeabb72856b798deb64c064f647c51a5da4
LeeJU <101004585+LeeJU0912@users.noreply.github.com>
2022-05-10 03:18:48 +0900

    [Bronze V] Title: A+B - 2, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+    int a;
+    int b;
+
+    scanf("%d", &a);
+    scanf("%d", &b);
+
+    if (a > 0, b < 10) {
+        printf("%d", a + b);
+    }
+
+    return 0;
+}
