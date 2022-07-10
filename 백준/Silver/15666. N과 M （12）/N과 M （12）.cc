9e47b19883f28f3c140df28b6cf793ce988acb6c
LeeJU <101004585+LeeJU0912@users.noreply.github.com>
2022-07-10 23:06:49 +0900

    [Silver II] Title: N과 M (12), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <cmath>
+#include <ctime>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <string>
+#include <vector>
+#include <tuple>
+#include <functional>
+#include <map>
+#include <set>
+#include <cstring>
+#include <array>
+#include <functional>
+#include <climits>
+
+using namespace std;
+
+int n, m;
+int arr[9];
+int save[9];
+
+void dfs(int num, int start) {
+	if (start == m) {
+		for (int i = 0; i < m; i++) {
+			cout << arr[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+	for (int i = num; i <= n; i++) {
+		if (save[i] == save[i + 1]) {
+			continue;
+		}
+		arr[start] = save[i];
+		dfs(i, start + 1);
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	
+	cin >> n >> m;
+
+	for (int i = 1; i <= n; i++) {
+		cin >> save[i];
+	}
+	sort(save + 1, save + n + 1);	
+
+	dfs(1, 0);
+
+	return 0;
+}
