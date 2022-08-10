3feafe8b43d05cb81bc5d4b0dd6406b7dc1af71e
LeeJU <101004585+LeeJU0912@users.noreply.github.com>
2022-08-10 13:47:02 +0900

    [Gold III] Title: Dance Dance Revolution, Time: 28 ms, Memory: 20116 KB -BaekjoonHub

---
+++ 백준/Gold/2342. Dance Dance Revolution/Dance Dance Revolution.cc
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <cmath>
+#include <ctime>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <string>
+#include <vector>
+#include <tuple>
+#include <functional>
+#include <map>
+#include <set>
+#include <cstring>
+#include <array>
+#include <climits>
+
+using namespace std;
+
+vector<int> v;
+
+int dp[100001][5][5];
+
+int check(int from, int to) {
+    if (from == 0) return 2;
+    if (from == to) return 1;
+    if (abs(from - to) == 2) return 4;
+
+    return 3;
+}
+
+int solve(int idx, int left, int right) {
+    if (idx == v.size()) return 0;
+
+    int& result = dp[idx][left][right];
+
+    if (result != -1) return result;
+
+    int L = solve(idx + 1, v[idx], right) + check(left, v[idx]);
+    int R = solve(idx + 1, left, v[idx]) + check(right, v[idx]);
+
+    return result = min(L, R);
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    int N = 1;
+
+    int i = 0;
+
+    while (1) {
+        cin >> N;
+        if (N == 0) {
+            break;
+
+        }
+        v.push_back(N);
+        i++;
+    }
+
+    memset(dp, -1, sizeof(dp));
+
+    cout << solve(0, 0, 0);
+
+    return 0;
+}
