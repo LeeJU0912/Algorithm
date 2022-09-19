52a9c8d386a86bf081f9750c48a148a8c2aedfc8
LeeJU <101004585+LeeJU0912@users.noreply.github.com>
2022-09-20 02:05:04 +0900

    [Bronze II] Title: 팰린드롬인지 확인하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <cmath>
+#include <ctime>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <string>
+#include <vector>
+#include <tuple>
+#include <functional>
+#include <map>
+#include <set>
+#include <cstring>
+#include <array>
+#include <climits>
+#include <cstdlib>
+#include <utility>
+#include <regex>
+#include <numeric>
+#include <cctype>
+#include <iomanip>
+#include <bitset>
+#include <sstream>
+#include <typeinfo>
+#include <cstdlib>
+
+using namespace std;
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+
+    string s;
+    cin >> s;
+
+    string ans;
+
+    for (int i = 0; i < s.length() / 2 + 1; i++) {
+        if (s[i] != s[s.length() - i - 1]) {
+            cout << 0;
+            return 0;
+        }
+    }
+
+    cout << 1;
+
+    return 0;
+}
