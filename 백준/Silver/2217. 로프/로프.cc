2691c4acae7014a51a733a276aec8f5917f80f2a
LeeJU <101004585+LeeJU0912@users.noreply.github.com>
2022-09-20 08:34:48 +0900

    [Silver IV] Title: 로프, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cmath>
+#include <ctime>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <string>
+#include <vector>
+#include <tuple>
+#include <functional>
+#include <map>
+#include <set>
+#include <cstring>
+#include <array>
+#include <climits>
+#include <cstdlib>
+#include <utility>
+#include <regex>
+#include <numeric>
+#include <cctype>
+#include <iomanip>
+#include <bitset>
+#include <sstream>
+#include <typeinfo>
+#include <cstdlib>
+
+using namespace std;
+
+int N;
+int a[100000];
+int ans;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        cin >> a[i];
+    }
+
+    sort(a, a + N, greater<>());
+
+    for (int i = 0; i < N; i++) {
+        ans = max(ans, a[i] * (i + 1));
+    }
+
+    cout << ans;
+
+    return 0;
+}
