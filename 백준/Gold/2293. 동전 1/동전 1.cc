9f7e3baa0938d4b6398f68737ba70a2072beae35
LeeJU <101004585+LeeJU0912@users.noreply.github.com>
2022-10-18 05:19:37 +0900

    [Gold V] Title: 동전 1, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <cmath>
+#include <ctime>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <string>
+#include <vector>
+#include <tuple>
+#include <functional>
+#include <map>
+#include <set>
+#include <cstring>
+#include <array>
+#include <climits>
+#include <cstdlib>
+#include <utility>
+#include <regex>
+#include <numeric>
+#include <cctype>
+#include <iomanip>
+#include <bitset>
+#include <sstream>
+#include <typeinfo>
+#include <cstdlib>
+#include <unordered_map>
+#include <cfloat>
+
+using namespace std;
+
+int n, k;
+int coin[101];
+int dp[10001];
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+
+    cin >> n >> k;
+
+    for (int i = 0; i < n; i++) {
+        cin >> coin[i];
+    }
+
+    dp[0] = 1;
+    for (int i = 0; i < n; i++) {
+        for (int j = coin[i]; j <= k; j++) {
+            dp[j] += dp[j - coin[i]];
+        }
+    }
+
+    cout << dp[k];
+
+    return 0;
+}
