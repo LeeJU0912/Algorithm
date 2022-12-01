3f892fd8d7d8b039f390808abec266141c299357
LeeJU <101004585+LeeJU0912@users.noreply.github.com>
2022-12-01 18:58:24 +0900

    [Silver V] Title: K번째 수, Time: 1088 ms, Memory: 21552 KB -BaekjoonHub

---
+++ 백준/Silver/11004. K번째 수/K번째 수.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+
+    int N, K;
+    cin >> N >> K;
+
+    vector<int> v(N);
+
+    for (int i = 0; i < N; i++) {
+        cin >> v[i];
+    }
+    sort(v.begin(), v.end());
+
+    cout << v[K - 1];
+
+    return 0;
+}
