1df8e61d105528a2018cc27128c1ed15f8e3ac4c
LeeJU <101004585+LeeJU0912@users.noreply.github.com>
2022-12-26 05:58:20 +0900

    [Bronze V] Title: 홍익대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+
+    int N;
+    cin >> N;
+
+    cout << N - 1946;
+
+    return 0;
+}
