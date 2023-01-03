ca40033badc03970948a7d66e6c922e6e5904f51
LeeJU <101004585+LeeJU0912@users.noreply.github.com>
2023-01-03 21:54:01 +0900

    [Silver II] Title: 차이를 최대로, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/10819. 차이를 최대로/차이를 최대로.cc
+++ 백준/Silver/10819. 차이를 최대로/차이를 최대로.cc
@@ -1,46 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N;
-
-vector<int> v;
-bool visited[8];
-vector<int> tmp;
-
-int ans;
-
-void solve() {
-    if (tmp.size() == N) {
-        int temp = 0;
-        for (int i = 1; i < N; i++) {
-            temp += abs(tmp[i - 1] - tmp[i]);
-        }
-        ans = max(ans, temp);
-    }
-
-    for (int i = 0; i < N; i++) {
-        if (!visited[i]) {
-            visited[i] = true;
-            tmp.push_back(v[i]);
-            solve();
-            tmp.pop_back();
-            visited[i] = false;
-        }
-    }
-}
-
 int main() {
     ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 
+    int ans = 0;
+    int N;
+
     cin >> N;
 
+    vector<int> v(N);
     for (int i = 0; i < N; i++) {
-        int temp;
-        cin >> temp;
-        v.push_back(temp);
+        cin >> v[i];
     }
 
-    solve();
+    sort(v.begin(), v.end());
+
+    do {
+        int temp = 0;
+        for (int i = 1; i < N; i++) {
+            temp += abs(v[i - 1] - v[i]);
+        }
+        ans = max(ans, temp);
+    } while(next_permutation(v.begin(), v.end()));
 
     cout << ans;
 
