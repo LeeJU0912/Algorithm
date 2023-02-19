057d3552991fd51fc23dcf5c3e1946a799dadcae
LeeJU <101004585+LeeJU0912@users.noreply.github.com>
2023-02-19 22:54:02 +0900

    [Bronze I] Title: 배고픈 아리의 샌드위치 구매하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/25166. 배고픈 아리의 샌드위치 구매하기/배고픈 아리의 샌드위치 구매하기.cc
+++ 백준/Bronze/25166. 배고픈 아리의 샌드위치 구매하기/배고픈 아리의 샌드위치 구매하기.cc
@@ -1,20 +1,41 @@
-#include <iostream>
-#include <bitset>
-
+#include <bits/stdc++.h>
+#define FastIO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 using namespace std;
 
-int S, M;
-
 int main() {
-    int coin = 0b1111111111;
+    FastIO
+
+    int S, M;
     cin >> S >> M;
 
-    if (S <= 0b1111111111) cout << "No thanks";
+    if (S < 1024) {
+        cout << "No thanks";
+        return 0;
+    }
+    
+    int money = 512;
+
+    vector<int> kugi;
+
+    while(money > 0 && M > 0) {
+        if (M >= money) {
+            M -= money;
+            kugi.push_back(money);
+        }
+        money /= 2;
+    }
+
+    money = S - 1023;
+
+    for (int i = 0; i < kugi.size(); i++) {
+        if (money >= kugi[i]) money -= kugi[i];
+    }
+
+    if (money) {
+        cout << "Impossible";
+    }
     else {
-        coin += M;
-        int ans = S ^ coin;
-        if (ans == 0) cout << "Thanks";
-        else cout << "Impossible";
+        cout << "Thanks";
     }
 
     return 0;
