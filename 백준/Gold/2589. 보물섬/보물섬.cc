4b0a2bcd3f0d163ea33e363aacf7f41e52111212
LeeJU <101004585+LeeJU0912@users.noreply.github.com>
2023-03-02 09:56:41 +0900

    [Gold V] Title: 보물섬, Time: 132 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Gold/2589. 보물섬/보물섬.cc
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+#define FastIO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+using namespace std;
+
+int N, M;
+bool graph[51][51];
+bool visited[51][51];
+int ans;
+
+int dx[4] = {0, 0, 1, -1};
+int dy[4] = {1, -1, 0, 0};
+
+void bfs(int x, int y) {
+    memset(visited, false, sizeof(visited));
+    queue<pair<pair<int,int>, int>> q;
+    q.push({{x, y}, 0});
+    visited[x][y] = true;
+
+    while(!q.empty()) {
+        auto now = q.front();
+        q.pop();
+
+        ans = max(ans, now.second);
+
+        for (int i = 0; i < 4; i++) {
+            int next_x = now.first.first + dx[i];
+            int next_y = now.first.second + dy[i];
+
+            if (next_x < 0 || next_x >= N || next_y < 0 || next_y >= M) continue;
+            if (visited[next_x][next_y]) continue;
+            if (!graph[next_x][next_y]) continue;
+            visited[next_x][next_y] = true;
+            q.push({{next_x, next_y}, now.second + 1});
+        }
+    }
+}
+
+int main() {
+    FastIO
+
+    cin >> N >> M;
+
+    for (int i = 0; i < N; i++) {
+        string s;
+        cin >> s;
+
+        for (int j = 0; j < M; j++) {
+            if (s[j] == 'L') graph[i][j] = true;
+        }
+    }
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            if (graph[i][j]) bfs(i, j);
+        }
+    }
+
+    cout << ans;
+
+    return 0;
+}
