4c394153deebd75cd41d597e258c816f295c5871
LeeJU <101004585+LeeJU0912@users.noreply.github.com>
2023-03-19 22:45:42 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define FastIO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+using namespace std;
+
+int main(){
+    FastIO
+
+    int A, B;
+
+    cin >> A >> B;
+
+    cout << A * B;
+
+    return 0;
+}
