551b01562f3b400eb984894a1f85ba3192ea2b89
LeeJU <101004585+LeeJU0912@users.noreply.github.com>
2023-05-03 16:50:54 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 40 ms, Memory: 2996 KB -BaekjoonHub

--- 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.cc
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.cc
@@ -1,48 +1,37 @@
-#include <iostream>
-#include <cmath>
-#include <ctime>
-#include <algorithm>
-#include <stack>
-#include <queue>
-#include <deque>
-#include <string>
-#include <vector>
-#include <tuple>
-#include <functional>
-#include <map>
-#include <set>
-#include <cstring>
-#include <array>
-#include <climits>
-
+#include <bits/stdc++.h>
+#define FastIO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 using namespace std;
 
 int N;
-int sero[501];
-int garo[501];
-
-int DP[501][501];
+vector<pair<int, int>> save;
+int DP[500][500];
 
 int main() {
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    FastIO
 
     cin >> N;
 
-    for (int i = 1; i <= N; i++) {
-        cin >> sero[i] >> garo[i];
+    for (int i = 0; i < N; i++) {
+        int a, b;
+        cin >> a >> b;
+        save.emplace_back(a, b);
     }
 
-    for (int i = 1; i < N; i++) { // 구간 범위의 크기 (i 사이의 값)
-        for (int j = 1; i + j <= N; j++) { // 구간의 시작점
-            DP[j][i + j] = INT_MAX;
-            for (int k = j; k <= i + j; k++) { // 구간을 반으로 나누는 부분
-                DP[j][i + j] = min(DP[j][i + j], DP[j][k] + DP[k + 1][i + j] + sero[j] * garo[k] * garo[i + j]);
-                // 최솟값(계산 안한 값이 0이면 무조건 최소이므로 j~i+j 설정, j~k 구간 최소값 + k+1~i+j 구간 최소값 + [i~i+j구간 구하기]구간 시작 * k로 나뉘는 부분 * 구간 끝)
+    if (save.size() == 1) {
+        cout << 0;
+        return 0;
+    }
+
+    for (int i = 1; i < N; i++) {
+        for (int j = 0; i + j < N; j++) {
+            DP[j][j + i] = INT_MAX;
+            for (int k = j; k < j + i; k++) {
+                DP[j][j + i] = min(DP[j][j + i], DP[j][k] + DP[k + 1][j + i] + save[j].first * save[k].second * save[j + i].second);
             }
         }
     }
 
-    cout << DP[1][N];
+    cout << DP[0][N - 1];
 
     return 0;
 }
