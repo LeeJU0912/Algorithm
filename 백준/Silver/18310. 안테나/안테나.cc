70eaf00d278d0612e40c3482781a6ab80674721b
LeeJU <101004585+LeeJU0912@users.noreply.github.com>
2023-05-05 00:14:03 +0900

    [Silver III] Title: 안테나, Time: 32 ms, Memory: 4464 KB -BaekjoonHub

---
+++ 백준/Silver/18310. 안테나/안테나.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define FastIO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+using namespace std;
+
+int N;
+vector<int> save;
+int hap[200000];
+
+int main() {
+    FastIO
+
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        int temp;
+        cin >> temp;
+        save.push_back(temp);
+    }
+
+    sort(save.begin(), save.end());
+
+    cout << save[(N - 1) / 2];
+
+    return 0;
+}
