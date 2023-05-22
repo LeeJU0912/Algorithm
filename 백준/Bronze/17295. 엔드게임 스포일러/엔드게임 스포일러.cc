c774587ca3a8bd9e0b8c437e65bf2308847c7394
LeeJU <101004585+LeeJU0912@users.noreply.github.com>
2023-05-23 00:41:29 +0900

    [Bronze V] Title: 엔드게임 스포일러, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/17295. 엔드게임 스포일러/엔드게임 스포일러.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define FastIO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+using namespace std;
+
+int main() {
+    FastIO
+
+    string s;
+    cin >> s;
+    
+    cout << "Avengers: Endgame";
+
+    return 0;
+}
