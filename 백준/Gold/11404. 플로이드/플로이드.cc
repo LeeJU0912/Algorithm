5c97b5bb3b7594e5b5bc47ba40b42089434d5006
LeeJU <101004585+LeeJU0912@users.noreply.github.com>
2023-05-25 15:05:22 +0900

    [Gold IV] Title: 플로이드, Time: 28 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.cc
+++ 백준/Gold/11404. 플로이드/플로이드.cc
@@ -1,72 +1,52 @@
-#include <iostream>
-#include <cmath>
-#include <ctime>
-#include <algorithm>
-#include <stack>
-#include <queue>
-#include <deque>
-#include <string>
-#include <vector>
-#include <tuple>
-#include <functional>
-#include <map>
-#include <set>
-#include <cstring>
-#include <array>
-#include <functional>
-#include <climits>
-
+#include <bits/stdc++.h>
+#define FastIO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 using namespace std;
 
 int n, m;
-int city[101][101];
-
-void Floyd() {
-	for (int m = 1; m <= n; m++) {
-		for (int s = 1; s <= n; s++) {
-			for (int e = 1; e <= n; e++) {
-				if (city[s][e] > city[s][m] + city[m][e]) {
-					city[s][e] = city[s][m] + city[m][e];
-				}
-			}
-		}
-	}
-}
+int graph[101][101];
 
 int main() {
-	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-	
-	cin >> n;
-	cin >> m;
+    FastIO
 
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= n; j++) {
-			city[i][j] = 1000000000;
-		}
-	}
+    cin >> n >> m;
 
-	for (int i = 0; i < m; i++) {
-		int a, b, c;
-		cin >> a >> b >> c;
-		if (city[a][b] < c) {
-			continue;
-		}
-		city[a][b] = c;
-	}
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            graph[i][j] = 100000000;
+        }
+    }
 
-	Floyd();
+    for (int i = 0; i < m; i++) {
+        int a, b, c;
+        cin >> a >> b >> c;
 
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= n; j++) {
-			if (i == j || city[i][j] == 1000000000) {
-				cout << "0" << ' ';
-			}
-			else {
-				cout << city[i][j] << ' ';
-			}
-		}
-		cout << '\n';
-	}
+        graph[a][b] = min(graph[a][b], c);
+    }
 
-	return 0;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            for (int k = 1; k <= n; k++) {
+                if (j == k) {
+                    graph[j][k] = 0;
+                }
+                else {
+                    graph[j][k] = min(graph[j][k], graph[j][i] + graph[i][k]);
+                }
+            }
+        }
+    }
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (graph[i][j] == 100000000) {
+                cout << 0 << ' ';
+            }
+            else {
+                cout << graph[i][j] << ' ';
+            }
+        }
+        cout << '\n';
+    }
+
+    return 0;
 }
