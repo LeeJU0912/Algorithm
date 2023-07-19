ca97aa93ebcb3528a7ee2c275206c0d25241d15c
LeeJU <101004585+LeeJU0912@users.noreply.github.com>
2023-07-19 10:18:33 +0900

    [Silver I] Title: 가장 가까운 세 사람의 심리적 거리, Time: 1028 ms, Memory: 5300 KB -BaekjoonHub

---
+++ 백준/Silver/20529. 가장 가까운 세 사람의 심리적 거리/가장 가까운 세 사람의 심리적 거리.cc
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+#define FastIO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+using namespace std;
+
+map<pair<string, string>, int> DP;
+
+vector<string> MBTI = {
+        "ISTJ",
+        "ISFJ",
+        "INFJ",
+        "INTJ",
+        "ISTP",
+        "ISFP",
+        "INFP",
+        "INTP",
+        "ESTP",
+        "ESFP",
+        "ENFP",
+        "ENTP",
+        "ESTJ",
+        "ESFJ",
+        "ENFJ",
+        "ENTJ"
+};
+
+int diff(string a, string b) {
+    int count = 0;
+    for (int i = 0; i < 4; i++) {
+        if (a[i] != b[i]) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+int main() {
+    FastIO
+
+    for (int i = 0; i < 15; i++) {
+        for (int j = i + 1; j < 16; j++) {
+            DP[{MBTI[i], MBTI[j]}] = diff(MBTI[i], MBTI[j]);
+            DP[{MBTI[j], MBTI[i]}] = DP[{MBTI[i], MBTI[j]}];
+        }
+    }
+
+
+    int T;
+    cin >> T;
+
+    while(T--) {
+        int n;
+        cin >> n;
+
+        vector<string> save(n);
+
+        for (int i = 0; i < n; i++) {
+            cin >> save[i];
+        }
+
+        int ans = INT_MAX;
+
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 1; j < n - 1; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    ans = min(ans, DP[{save[i], save[j]}] + DP[{save[j], save[k]}] + DP[{save[i], save[k]}]);
+                    if (ans == 0) break;
+                }
+                if (ans == 0) break;
+            }
+            if (ans == 0) break;
+        }
+
+        cout << ans << '\n';
+    }
+
+    return 0;
+}
