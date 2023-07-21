a93da40ef973bfa5567011690d6b74656fa54b03
LeeJU <101004585+LeeJU0912@users.noreply.github.com>
2023-07-22 01:42:36 +0900

    [Bronze IV] Title: 베라의 패션, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define FastIO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+using namespace std;
+
+int main() {
+    FastIO
+
+    int N;
+    cin >> N;
+
+    cout << N * N - N;
+
+    return 0;
+}
