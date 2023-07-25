f15e6de56d483abd074266c4f91801e6b409b066
LeeJU <101004585+LeeJU0912@users.noreply.github.com>
2023-07-26 00:11:40 +0900

    [Gold V] Title: 치킨 배달, Time: 4 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.cc
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.cc
@@ -1,81 +1,66 @@
-#include <iostream>
-#include <cmath>
-#include <ctime>
-#include <algorithm>
-#include <stack>
-#include <queue>
-#include <deque>
-#include <string>
-#include <vector>
-#include <tuple>
-#include <functional>
-#include <map>
-#include <set>
-#include <cstring>
-#include <array>
-#include <climits>
-#include <cstdlib>
-#include <utility>
-
+#include<bits/stdc++.h>
+#define FastIO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 using namespace std;
 
 int N, M;
-int city[51][51];
-vector<pair<int, int>> house;
+int board[51][51];
+bool visited[13];
 vector<pair<int, int>> chicken;
-int visited[13];
-int result = INT_MAX;
+vector<pair<int, int>> house;
 
-int distance(pair<int, int> a, pair<int, int> b) {
-    return abs(a.first - b.first) + abs(a.second - b.second);
-}
+int ans = INT_MAX;
 
 void solve(int idx, int counter) {
     if (counter == M) {
         int temp = 0;
         for (int i = 0; i < house.size(); i++) {
-            int dist = INT_MAX;
+            int mini = INT_MAX;
+
             for (int j = 0; j < chicken.size(); j++) {
-                if (visited[j]) {
-                    dist = min(dist, distance(house[i], chicken[j]));
-                }
+                if (!visited[j]) continue;
+                mini = min(mini, abs(chicken[j].first - house[i].first) + abs(chicken[j].second - house[i].second));
             }
-            temp += dist;
+
+            temp += mini;
         }
-        result = min(result, temp);
+
+        ans = min(ans, temp);
         return;
     }
-
-    if (idx == chicken.size()) {
-        return;
+    for (int i = idx + 1; i < chicken.size(); i++) {
+        visited[i] = true;
+        solve(i, counter + 1);
+        visited[i] = false;
     }
-
-    visited[idx] = true;
-    solve(idx + 1, counter + 1);
-    visited[idx] = false;
-    solve(idx + 1, counter);
 }
 
 int main() {
-    ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+    FastIO
 
     cin >> N >> M;
 
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
-            cin >> city[i][j];
-            if (city[i][j] == 1) {
-                house.push_back({i , j});
+            cin >> board[i][j];
+
+            if (board[i][j] == 1) {
+                house.push_back({i, j});
             }
-            else if (city[i][j] == 2) {
-                chicken.push_back({i , j});
+            else if (board[i][j] == 2) {
+                chicken.push_back({i, j});
             }
         }
     }
 
-    solve(0, 0);
+    for (int i = 0; i < chicken.size(); i++) {
+        visited[i] = true;
+        solve(i, 1);
+        visited[i] = false;
+    }
 
-    cout << result;
+
+
+    cout << ans;
 
     return 0;
 }
