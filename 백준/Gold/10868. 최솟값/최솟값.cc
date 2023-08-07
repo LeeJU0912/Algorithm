0ad502b49e123a2fd5441bad8d1dd786fa660964
LeeJU <101004585+LeeJU0912@users.noreply.github.com>
2023-08-07 12:14:14 +0900

    [Gold I] Title: 최솟값, Time: 80 ms, Memory: 4984 KB -BaekjoonHub

---
+++ 백준/Gold/10868. 최솟값/최솟값.cc
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+#define FastIO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+using namespace std;
+
+int N;
+vector<int> save;
+int M;
+
+vector<long long> tree;
+
+void init(int node, int left, int right) {
+    if (left == right) {
+        tree[node] = save[left];
+        return;
+    }
+
+    init(node * 2, left, (left + right) / 2);
+    init(node * 2 + 1, (left + right) / 2 + 1, right);
+
+    tree[node] = min(tree[node * 2], tree[node * 2 + 1]);
+}
+
+void update(int node, int left, int right, int target, long long val) {
+    if (target < left || target > right) return;
+    if (left == right) {
+        save[target] = val;
+        tree[node] = val;
+        return;
+    }
+
+    update(node * 2, left, (left + right) / 2, target, val);
+    update(node * 2 + 1, (left + right) / 2 + 1, right, target, val);
+
+    tree[node] = min(tree[node * 2], tree[node * 2 + 1]);
+}
+
+long long query(int node, int left, int right, int start, int end) {
+    if (left > end || right < start) return LLONG_MAX;
+    if (start <= left && right <= end) return tree[node];
+
+    long long l = query(node * 2, left, (left + right) / 2, start, end);
+    long long r = query(node * 2 + 1, (left + right) / 2 + 1, right, start, end);
+
+    return min(l, r);
+}
+
+int main() {
+    FastIO
+
+    cin >> N >> M;
+
+    int tree_depth = (int)ceil(log2(N));
+    int tree_size = (1 << (tree_depth + 1));
+    tree = vector<long long>(tree_size);
+
+    for (int i = 0; i < N; i++) {
+        int temp;
+        cin >> temp;
+        save.push_back(temp);
+    }
+
+    init(1, 0, N - 1);
+
+    for (int i = 0; i < M; i++) {
+        int a, b;
+        cin >> a >> b;
+
+        cout << query(1, 0, N - 1, a - 1, b - 1) << '\n';
+    }
+
+    return 0;
+}
