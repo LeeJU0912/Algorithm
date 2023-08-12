c0ad8dac339744a7e6464c1fee013662b200521d
LeeJU <101004585+LeeJU0912@users.noreply.github.com>
2023-08-12 09:32:52 +0900

    [Gold IV] Title: 운동, Time: 104 ms, Memory: 2648 KB -BaekjoonHub

--- 백준/Gold/1956. 운동/운동.cc
+++ 백준/Gold/1956. 운동/운동.cc
@@ -1,66 +1,54 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 #define FastIO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 using namespace std;
 
 int V, E;
+int graph[401][401];
 
-vector<pair<int, int>> graph[401];
-
-int dist[401][401];
-
-void dijkstra(int start) {
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
-    pq.emplace(0, start);
-    dist[start][start] = 0;
-
-    while(!pq.empty()) {
-        auto now = pq.top();
-        pq.pop();
-
-        for (int i = 0; i < graph[now.second].size(); i++) {
-            auto next = graph[now.second][i];
-
-            if (dist[start][next.second] > now.first + next.first) {
-                dist[start][next.second] = now.first + next.first;
-                pq.emplace(dist[start][next.second], next.second);
-            }
-        }
-    }
-}
 
 int main() {
     FastIO
 
     cin >> V >> E;
 
+    for (int i = 1; i <= V; i++) {
+        for (int j = 1; j <= V; j++) {
+            if (i == j) continue;
+            graph[i][j] = 1e9;
+        }
+    }
+
     for (int i = 0; i < E; i++) {
         int a, b, c;
         cin >> a >> b >> c;
-        graph[a].emplace_back(c, b);
+        graph[a][b] = c;
     }
 
     for (int i = 1; i <= V; i++) {
         for (int j = 1; j <= V; j++) {
-            dist[i][j] = INT_MAX;
-        }
-        dijkstra(i);
-    }
-
-    int ans = INT_MAX;
-
-    for (int i = 1; i < V; i++) {
-        for (int j = i + 1; j <= V; j++) {
-            if (dist[i][j] == INT_MAX || dist[j][i] == INT_MAX) continue;
-            ans = min(ans, dist[i][j] + dist[j][i]);
+            for (int k = 1; k <= V; k++) {
+                if (graph[j][k] > graph[j][i] + graph[i][k]) {
+                    graph[j][k] = graph[j][i] + graph[i][k];
+                }
+            }
         }
     }
 
-    if (ans == INT_MAX) {
+    int ans = 1e9;
+    for (int i = 1; i <= V; i++) {
+        for (int j = 1; j <= V; j++) {
+            if (i == j) continue;
+            if (graph[i][j] == 1e9 || graph[j][i] == 1e9) continue;
+            ans = min(ans, graph[i][j] + graph[j][i]);
+        }
+    }
+
+    if (ans == 1e9) {
         cout << -1;
     }
     else {
         cout << ans;
     }
-
+    
     return 0;
 }
