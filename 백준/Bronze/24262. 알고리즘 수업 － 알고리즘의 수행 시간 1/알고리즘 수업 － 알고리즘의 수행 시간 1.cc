853742d02a16ecefae71f015b4a35f6f7ddeb38a
LeeJU <101004585+LeeJU0912@users.noreply.github.com>
2023-12-24 06:01:23 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define FastIO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+using namespace std;
+
+
+int main() {
+    FastIO
+
+    int n;
+    cin >> n;
+
+    cout << 1 << '\n' << 0;
+
+    return 0;
+}
