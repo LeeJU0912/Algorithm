ff87e589d7dce7a7901915c4b47b8487189fb6da
LeeJU <101004585+LeeJU0912@users.noreply.github.com>
2023-12-25 00:49:03 +0900

    [Bronze IV] Title: 녹색거탑, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define FastIO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+using namespace std;
+
+int main() {
+    FastIO
+
+    int N;
+    cin >> N;
+
+    cout << pow(2, N);
+
+    return 0;
+}
