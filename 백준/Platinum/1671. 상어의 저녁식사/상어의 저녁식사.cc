5f1b01417a6bc4dea5aa82d526c0bd1f767d5a06
LeeJU <101004585+LeeJU0912@users.noreply.github.com>
2024-01-09 15:22:18 +0900

    [Platinum III] Title: 상어의 저녁식사, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Platinum/1671. 상어의 저녁식사/상어의 저녁식사.cc
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+#define FastIO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+using namespace std;
+
+int N;
+
+vector<pair<int, pair<int, int>>> save;
+
+vector<int> graph[51];
+
+int matching[51];
+bool visited[51];
+
+int ans;
+
+int comp(int a, int b) {
+    if (save[a].first == save[b].first && save[a].second.first == save[b].second.first && save[a].second.second == save[b].second.second) return 2;
+    else if (save[a].first >= save[b].first && save[a].second.first >= save[b].second.first && save[a].second.second >= save[b].second.second) return 1;
+    else if (save[a].first <= save[b].first && save[a].second.first <= save[b].second.first && save[a].second.second <= save[b].second.second) return 0;
+    else return -1;
+}
+
+bool dfs(int A) {
+    if (visited[A]) return false;
+    visited[A] = true;
+
+    for (int B : graph[A]) {
+        if (matching[B] == -1 || dfs(matching[B])) {
+            matching[B] = A;
+            return true;
+        }
+    }
+    return false;
+}
+
+int main() {
+    FastIO
+
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        save.push_back({a, {b, c}});
+    }
+
+    for (int i = 0; i < N - 1; i++) {
+        for (int j = i + 1; j < N; j++) {
+            int val = comp(i, j);
+            if (val == 2) graph[j].push_back(i);
+            else if (val == 1) graph[i].push_back(j);
+            else if (val == 0) graph[j].push_back(i);
+        }
+    }
+
+    memset(matching, -1, sizeof(matching));
+    for (int i = 0; i < N; i++) {
+        memset(visited, false, sizeof(visited));
+        if (dfs(i)) ans++;
+        memset(visited, false, sizeof(visited));
+        if (dfs(i)) ans++;
+    }
+
+    cout << N - ans;
+
+    return 0;
+}
