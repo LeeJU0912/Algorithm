0a42088b05dab7a8383c044116299cfa42f1ac0b
LeeJU <101004585+LeeJU0912@users.noreply.github.com>
2024-01-30 23:18:11 +0900

    [Gold IV] Title: 사냥꾼, Time: 0 ms, Memory: 2024 KB, Score: 41 point -BaekjoonHub

---
+++ 백준/Gold/8983. 사냥꾼/사냥꾼.cc
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#define FastIO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+using namespace std;
+
+int M, N, L;
+
+vector<int> shot;
+vector<pair<int, int>> pigs;
+
+int ans;
+
+int main() {
+    FastIO
+
+    cin >> M >> N >> L;
+
+    for (int i = 0; i < M; i++) {
+        int temp;
+        cin >> temp;
+        shot.push_back(temp);
+    }
+
+    sort(shot.begin(), shot.end());
+
+    for (int i = 0; i < N; i++) {
+        int a, b;
+        cin >> a >> b;
+        pigs.push_back({a, b});
+    }
+
+    sort(pigs.begin(), pigs.end());
+
+    for (int i = 0; i < N; i++) {
+        if (L < pigs[i].second) continue;
+        int mini = max(0, pigs[i].first - (L - pigs[i].second));
+        int maxi = min(1000000001, pigs[i].first + (L - pigs[i].second));
+        
+        int sch = lower_bound(shot.begin(), shot.end(), mini) - shot.begin();
+        if (shot[sch] <= maxi) ans++;
+    }
+    
+    cout << ans;
+    
+    return 0;
+}
