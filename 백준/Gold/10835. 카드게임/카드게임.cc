726241e54d8eecf0c8d1c1777700ce9dd2a70d58
LeeJU <101004585+LeeJU0912@users.noreply.github.com>
2024-03-07 02:57:19 +0900

    [Gold V] Title: 카드게임, Time: 20 ms, Memory: 17676 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/10835. 카드게임/카드게임.cc
+++ 백준/Gold/10835. 카드게임/카드게임.cc
@@ -7,52 +7,25 @@ int A[2001];
 int B[2001];
 int DP[2001][2001];
 
-int ans;
-
-void solve() {
-    queue<pair<int, pair<int, int>>> q;
-    q.push({0, {1, 1}});
-
-    while(!q.empty()) {
-        int now_counter = q.front().first;
-        int now_left = q.front().second.first;
-        int now_right = q.front().second.second;
-        q.pop();
-
-        if (DP[now_left][now_right] >= now_counter) continue;
-        DP[now_left][now_right] = now_counter;
-
-        ans = max(ans, now_counter);
-        if (now_left == N + 1 || now_right == N + 1) continue;
-
-        if (A[now_left] > B[now_right]) {
-            q.push({now_counter + B[now_right], {now_left, now_right + 1}});
-        }
-        q.push({now_counter, {now_left + 1, now_right + 1}});
-        q.push({now_counter, {now_left + 1, now_right}});
-    }
-}
-
 int main() {
     FastIO
 
     cin >> N;
-    for (int i = 1; i <= N; i++) {
+    for (int i = N; i >= 1; i--) {
         cin >> A[i];
     }
-    for (int i = 1; i <= N; i++) {
+    for (int i = N; i >= 1; i--) {
         cin >> B[i];
     }
 
     for (int i = 1; i <= N; i++) {
         for (int j = 1; j <= N; j++) {
-            DP[i][j] = -1;
+            DP[i][j] = max(DP[i - 1][j], DP[i - 1][j - 1]);
+            if (A[i] > B[j]) DP[i][j] = max(DP[i][j], DP[i][j - 1] + B[j]);
         }
     }
 
-    solve();
-
-    cout << ans;
+    cout << DP[N][N];
 
     return 0;
 }
