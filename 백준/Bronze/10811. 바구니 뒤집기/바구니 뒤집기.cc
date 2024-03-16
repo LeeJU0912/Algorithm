bf953a38c3fe5c8d9bcee88476a67b003672fe1d
LeeJU <101004585+LeeJU0912@users.noreply.github.com>
2024-03-17 00:48:53 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.cc
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.cc
@@ -2,34 +2,27 @@
 #define FastIO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 using namespace std;
 
+int N, M;
+int ans[101];
+
 int main() {
     FastIO
 
-    int N, M;
     cin >> N >> M;
-
-    vector<int> save(N + 1);
-
     for (int i = 1; i <= N; i++) {
-        save[i] = i;
+        ans[i] = i;
     }
-
     for (int i = 0; i < M; i++) {
         int a, b;
         cin >> a >> b;
 
-        stack<int> s;
-        for (int j = a; j <= b; j++) {
-            s.push(save[j]);
-        }
-        for (int j = a; j <= b; j++) {
-            save[j] = s.top();
-            s.pop();
+        for (int j = 0; j < (b - a + 1) / 2; j++) {
+            swap(ans[a + j], ans[b - j]);
         }
     }
 
     for (int i = 1; i <= N; i++) {
-        cout << save[i] << ' ';
+        cout << ans[i] << ' ';
     }
 
     return 0;
