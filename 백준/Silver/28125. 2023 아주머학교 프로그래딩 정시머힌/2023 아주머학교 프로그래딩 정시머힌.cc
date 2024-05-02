baf6213abf12a6f174c102b607f52a123afe1d21
LeeJU <101004585+LeeJU0912@users.noreply.github.com>
2024-05-02 23:05:23 +0900

    [Silver IV] Title: 2023 아주머학교 프로그래딩 정시머힌, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/28125. 2023 아주머학교 프로그래딩 정시머힌/2023 아주머학교 프로그래딩 정시머힌.cc
+++ 백준/Silver/28125. 2023 아주머학교 프로그래딩 정시머힌/2023 아주머학교 프로그래딩 정시머힌.cc
@@ -18,48 +18,40 @@ int main() {
         for (int j = 0; j < s.size(); j++) {
             if (j <= s.size() - 3 && (s[j] == '\\' && s[j + 1] == '\\' && s[j + 2] == '\'')) {
                 ans += 'w';
-                counter++;
                 j += 2;
             }
             else if (j <= s.size() - 2 && (s[j] == '\\' && s[j + 1] == '\'')) {
                 ans += 'v';
-                counter++;
                 j++;
             }
             else if (s[j] == '@') {
                 ans += 'a';
-                counter++;
             }
             else if (s[j] == '[') {
                 ans += 'c';
-                counter++;
             }
             else if (s[j] == '!') {
                 ans += 'i';
-                counter++;
             }
             else if (s[j] == ';') {
                 ans += 'j';
-                counter++;
             }
             else if (s[j] == '^') {
                 ans += 'n';
-                counter++;
             }
             else if (s[j] == '0') {
                 ans += 'o';
-                counter++;
             }
             else if (s[j] == '7') {
                 ans += 't';
-                counter++;
             }
             else {
                 ans += s[j];
+                counter++;
             }
         }
 
-        if (counter * 2 >= ans.size()) {
+        if (counter * 2 <= ans.size()) {
             cout << "I don't understand" << '\n';
         }
         else {
