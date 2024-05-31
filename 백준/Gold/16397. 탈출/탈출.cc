4cc48c26824a504ead237dc8bff3f9eedf418ef3
LeeJU <101004585+LeeJU0912@users.noreply.github.com>
2024-06-01 04:46:06 +0900

    [Gold IV] Title: 탈출, Time: 0 ms, Memory: 2676 KB -BaekjoonHub

--- 백준/Gold/16397. 탈출/탈출.cc
+++ 백준/Gold/16397. 탈출/탈출.cc
@@ -8,29 +8,38 @@ int DP[100000];
 int ans = INT_MAX;
 
 void solve(int counter, int now) {
-    if (now >= 100000) return;
-    if (DP[now] <= counter) return;
-    if (counter > T) return;
+    queue<pair<int, int>> q;
+    q.push({counter, now});
 
-    DP[now] = counter;
+    while(!q.empty()) {
+        int now_counter = q.front().first;
+        int now_number = q.front().second;
+        q.pop();
 
-    if (now == G) {
-        ans = min(ans, DP[now]);
-        return;
+        if (now_number >= 100000) continue;
+        if (DP[now_number] <= now_counter) continue;
+        if (now_counter > T) continue;
+
+        DP[now_number] = now_counter;
+
+        if (now_number == G) {
+            ans = min(ans, DP[now_number]);
+            continue;
+        }
+
+        q.push({now_counter + 1, now_number + 1});
+
+        if (now_number * 2 >= 100000 || now_number == 0) continue;
+
+        int next = now_number * 2;
+        int minus = 1;
+        while(next >= 10) {
+            next /= 10;
+            minus *= 10;
+        }
+
+        q.push({now_counter + 1, now_number * 2 - minus});
     }
-
-    solve(counter + 1, now + 1);
-
-    if (now * 2 >= 100000 || now == 0) return;
-
-    int next = now * 2;
-    int minus = 1;
-    while(next >= 10) {
-        next /= 10;
-        minus *= 10;
-    }
-
-    solve(counter + 1, now * 2 - minus);
 }
 
 int main() {
