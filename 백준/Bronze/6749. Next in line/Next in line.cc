a49887238114e6633bfd6d86e00599fe1cb18fb6
LeeJU <101004585+LeeJU0912@users.noreply.github.com>
2024-05-31 20:23:31 +0900

    [Bronze IV] Title: Next in line, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/6749. Next in line/Next in line.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define FastIO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+using namespace std;
+
+int main() {
+    FastIO
+
+    int Y, M;
+    cin >> Y >> M;
+
+    cout << (M - Y) + M;
+
+    return 0;
+}
