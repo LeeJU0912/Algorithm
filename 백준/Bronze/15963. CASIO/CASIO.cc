a15796bdbe260c77b58799ab223be72cc440a671
LeeJU <101004585+LeeJU0912@users.noreply.github.com>
2024-07-02 05:55:24 +0900

    [Bronze V] Title: CASIO, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15963. CASIO/CASIO.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define FastIO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+using namespace std;
+
+long long N, M;
+
+int main() {
+    FastIO
+
+    cin >> N >> M;
+
+    cout << (N == M ? 1 : 0);
+
+    return 0;
+}
