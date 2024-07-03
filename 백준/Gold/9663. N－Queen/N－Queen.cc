519fa860ee737e13838c52bf838eac469ca93210
LeeJU <101004585+LeeJU0912@users.noreply.github.com>
2024-07-03 22:34:41 +0900

    [Gold IV] Title: N-Queen, Time: 1348 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -1,58 +1,73 @@
-#include <iostream>
-#include <cmath>
-#include <ctime>
-#include <algorithm>
-#include <stack>
-#include <queue>
-#include <deque>
-#include <string>
-#include <vector>
-#include <tuple>
-#include <functional>
-#include <map>
-#include <set>
-#include <cstring>
-#include <array>
-#include <functional>
-
+#include<bits/stdc++.h>
+#define FastIO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 using namespace std;
 
-int n;
-int counter;
-int list[15];
+int N;
+int board[15][15];
+int chk[15][15];
+int ans;
 
-int compare(int start) {
-	for (int i = 0; i < start; i++) {
-		if (list[start] == list[i] || start - i == abs(list[start] - list[i])) {
-			return 0;
-		}
+void visit_plus(int x, int y) {
+	int counter = 1;
+	while(x + counter < N && y + counter < N) {
+		chk[x + counter][y + counter]++;
+		counter++;
 	}
-	return 1;
+	
+	counter = 1;
+	while(x + counter < N && y - counter >= 0) {
+		chk[x + counter][y - counter]++;
+		counter++;
+	}
+	
+	for (int i = x + 1; i < N; i++) {
+		chk[i][y]++;
+	}
 }
 
-void nqueen(int start) {
-
-	if (start == n) {
+void visit_minus(int x, int y) {
+	int counter = 1;
+	while(x + counter < N && y + counter < N) {
+		chk[x + counter][y + counter]--;
 		counter++;
+	}
+	
+	counter = 1;
+	while(x + counter < N && y - counter >= 0) {
+		chk[x + counter][y - counter]--;
+		counter++;
+	}
+	
+	for (int i = x + 1; i < N; i++) {
+		chk[i][y]--;
+	}
+}
+
+void solve(int x, int y) {
+	if(x == N - 1) {
+		ans++;
 		return;
 	}
-	for (int i = 0; i < n; i++) {
-		list[start] = i;
-		if (compare(start)) {
-			nqueen(start + 1);
-		}
+	for (int i = 0; i < N; i++) {
+		if (chk[x + 1][i]) continue;
+		visit_plus(x + 1, i);
+		solve(x + 1, i);
+		visit_minus(x + 1, i);
 	}
-	return;
 }
 
 int main() {
-	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-
-	cin >> n;
-
-	nqueen(0);
-
-	cout << counter;
+	FastIO
+	
+	cin >> N;
+	
+	for (int i = 0; i < N; i++) {
+		visit_plus(0, i);
+		solve(0, i);
+		visit_minus(0, i);
+	}
+	
+	cout << ans;
 	
 	return 0;
 }
