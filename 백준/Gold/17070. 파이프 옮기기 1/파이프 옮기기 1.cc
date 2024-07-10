0cf1004604cc4131311ed8e7701459a18ff81d16
LeeJU <101004585+LeeJU0912@users.noreply.github.com>
2024-07-11 02:10:25 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.cc
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.cc
@@ -1,78 +1,35 @@
-#include <iostream>
-#include <cmath>
-#include <ctime>
-#include <algorithm>
-#include <stack>
-#include <queue>
-#include <deque>
-#include <string>
-#include <vector>
-#include <tuple>
-#include <functional>
-#include <map>
-#include <set>
-#include <cstring>
-#include <array>
-#include <climits>
-#include <cstdlib>
-#include <utility>
-
+#include <bits/stdc++.h>
+#define FastIO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 using namespace std;
 
-int counter;
-int pipe[17][17];
 int N;
-
-void solve(int x, int y, int status) {
-    if (x == N - 1 && y == N - 1) {
-        counter++;
-    }
-    if (x > N - 1 || y > N - 1) return;
-
-    if (status == 0) {
-        if (pipe[x][y + 1] != 1) {
-            solve(x, y + 1, 0);
-        }
-
-        if (!(pipe[x + 1][y] == 1 || pipe[x][y + 1] == 1 || pipe[x + 1][y + 1] == 1)) {
-            solve(x + 1, y + 1, 2);
-        }
-    }
-    else if (status == 1) {
-        if (pipe[x + 1][y] != 1) {
-            solve(x + 1, y, 1);
-        }
-        if (!(pipe[x + 1][y] == 1 || pipe[x][y + 1] == 1 || pipe[x + 1][y + 1] == 1)) {
-            solve(x + 1, y + 1, 2);
-        }
-    }
-    else if (status == 2) {
-        if (pipe[x][y + 1] != 1) {
-            solve(x, y + 1, 0);
-        }
-        if (pipe[x + 1][y] != 1) {
-            solve(x + 1, y, 1);
-        }
-        if (!(pipe[x + 1][y] == 1 || pipe[x][y + 1] == 1 || pipe[x + 1][y + 1] == 1)) {
-            solve(x + 1, y + 1, 2);
-        }
-    }
-}
+bool board[17][17];
+int DP[17][17][3];
 
 int main() {
-    ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+    FastIO
 
     cin >> N;
-
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            cin >> pipe[i][j];
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= N; j++) {
+            cin >> board[i][j];
         }
     }
 
-    solve(0, 1, 0);
+    DP[1][2][2] = 1;
 
-    cout << counter;
+    for (int i = 1; i <= N; i++) {
+        for (int j = 3; j <= N; j++) {
+            if (board[i][j]) continue;
+            DP[i][j][0] = DP[i - 1][j][0] + DP[i - 1][j][1];
+            DP[i][j][2] = DP[i][j - 1][1] + DP[i][j - 1][2];
+
+            if (board[i][j] || board[i - 1][j] || board[i][j - 1]) continue;
+            DP[i][j][1] = DP[i - 1][j - 1][0] + DP[i - 1][j - 1][1] + DP[i - 1][j - 1][2];
+        }
+    }
+
+    cout << DP[N][N][0] + DP[N][N][1] + DP[N][N][2];
 
     return 0;
 }
