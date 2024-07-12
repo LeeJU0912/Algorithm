4293f3f132b34b2e770100957488e9d82edc3865
LeeJU <101004585+LeeJU0912@users.noreply.github.com>
2024-07-12 22:00:51 +0900

    [Silver II] Title: 회문수, Time: 96 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/30446. 회문수/회문수.cc
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+#define FastIO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+using namespace std;
+
+long long n;
+int ans;
+
+bool make_ans(int now, int plus) {
+    long long calc = now;
+    calc *= 10;
+    calc += plus;
+
+    int temp = now;
+    queue<int> stk;
+    while(temp) {
+        stk.push(temp % 10);
+        temp /= 10;
+    }
+
+    while(!stk.empty()) {
+        calc *= 10;
+        calc += stk.front();
+        stk.pop();
+    }
+
+    if (calc <= n) return true;
+    else return false;
+}
+
+void solve(int now, int count) {
+    if (count == 5) return;
+
+    long long calc = now;
+
+    int temp = now;
+    queue<int> stk;
+    while(temp) {
+        stk.push(temp % 10);
+        temp /= 10;
+    }
+
+    while(!stk.empty()) {
+        calc *= 10;
+        calc += stk.front();
+        stk.pop();
+    }
+    
+    if (calc <= n) ans++;
+    else return;
+
+    for (int i = 0; i <= 9; i++) {
+        if (make_ans(now, i)) {
+            ans++;
+            solve(now * 10 + i, count + 1);
+        }
+    }
+}
+
+int main() {
+    FastIO
+
+    cin >> n;
+
+    if (n <= 9) {
+        cout << n;
+        return 0;
+    }
+
+    for (int i = 1; i <= 9; i++) {
+        solve(i, 0);
+    }
+
+    cout << ans + 9;
+
+    return 0;
+}
