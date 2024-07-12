1873acd7b1174fba24faa7766a25e2be493a4966
LeeJU <101004585+LeeJU0912@users.noreply.github.com>
2024-07-12 21:23:52 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 40 ms, Memory: 6912 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.cc
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.cc
@@ -3,32 +3,33 @@
 using namespace std;
 
 int n, m;
-int start_x, start_y;
-int graph[1001][1001];
+bool board[1001][1001];
+int dist[1001][1001];
+int startX, startY;
+
 int dx[4] = {0, 0, 1, -1};
 int dy[4] = {1, -1, 0, 0};
-bool visited[1001][1001];
 
-void bfs() {
+void solve() {
     queue<pair<int, int>> q;
-    q.emplace(start_x, start_y);
-    visited[start_x][start_y] = true;
-    graph[start_x][start_y] = 0;
+    q.push({startX, startY});
+    dist[startX][startY] = 0;
 
     while(!q.empty()) {
-        auto now = q.front();
+        int now_x = q.front().first;
+        int now_y = q.front().second;
         q.pop();
 
         for (int i = 0; i < 4; i++) {
-            int next_x = now.first + dx[i];
-            int next_y = now.second + dy[i];
+            int next_x = now_x + dx[i];
+            int next_y = now_y + dy[i];
 
             if (next_x < 0 || next_x >= n || next_y < 0 || next_y >= m) continue;
-            if (visited[next_x][next_y]) continue;
-            if (graph[next_x][next_y] == 0) continue;
-            visited[next_x][next_y] = true;
-            graph[next_x][next_y] = graph[now.first][now.second] + 1;
-            q.emplace(next_x, next_y);
+            if (board[next_x][next_y]) continue;
+            if (dist[next_x][next_y] != -1) continue;
+
+            dist[next_x][next_y] = dist[now_x][now_y] + 1;
+            q.push({next_x, next_y});
         }
     }
 }
@@ -37,33 +38,31 @@ int main() {
     FastIO
 
     cin >> n >> m;
-
-
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            cin >> graph[i][j];
-            if (graph[i][j] == 2) {
-                start_x = i;
-                start_y = j;
+            dist[i][j] = -1;
+
+            int temp;
+            cin >> temp;
+
+            if (temp == 0) {
+                board[i][j] = true;
+            }
+            else if (temp == 2) {
+                startX = i;
+                startY = j;
             }
         }
     }
 
-    bfs();
+    solve();
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            if (!visited[i][j] && graph[i][j] != 0) {
-                graph[i][j] = -1;
-            }
-        }
-    }
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            cout << graph[i][j] << ' ';
+            cout << (board[i][j] ? 0 : dist[i][j]) << ' ';
         }
         cout << '\n';
     }
+
     return 0;
 }
