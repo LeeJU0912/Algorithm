ccfc716a955ead3cf67be2c0fb1cfa47dffce148
LeeJU <101004585+LeeJU0912@users.noreply.github.com>
2024-08-07 23:03:26 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define FastIO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+using namespace std;
+
+int main() {
+    FastIO
+
+    string s;
+    cin >> s;
+
+    cout << ":fan::fan::fan:\n"
+            ":fan::" << s << "::fan:\n"
+            ":fan::fan::fan:";
+
+    return 0;
+}
