cdc15ee164ca7111e593825457335a2688911c8d
LeeJU <101004585+LeeJU0912@users.noreply.github.com>
2024-08-12 23:47:14 +0900

    [Bronze IV] Title: 달달함이 넘쳐흘러, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/17256. 달달함이 넘쳐흘러/달달함이 넘쳐흘러.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define FastIO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+using namespace std;
+
+int main() {
+    FastIO
+
+    int a, b, c, d, e, f;
+    cin >> a >> b >> c >> d >> e >> f;
+
+    cout << d - c << ' ' << e / b << ' ' << f - a;
+
+    return 0;
+}
