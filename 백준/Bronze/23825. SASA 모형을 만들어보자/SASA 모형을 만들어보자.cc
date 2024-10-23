f919bc68c35c82e284b0ba73c0ec369ca4cf0ab7
LeeJU <101004585+LeeJU0912@users.noreply.github.com>
2024-10-23 23:59:18 +0900

    [Bronze IV] Title: SASA 모형을 만들어보자, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23825. SASA 모형을 만들어보자/SASA 모형을 만들어보자.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define FastIO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+using namespace std;
+
+int main() {
+    FastIO
+
+    int N, M;
+    cin >> N >> M;
+    cout << min(N, M) / 2;
+
+    return 0;
+}
