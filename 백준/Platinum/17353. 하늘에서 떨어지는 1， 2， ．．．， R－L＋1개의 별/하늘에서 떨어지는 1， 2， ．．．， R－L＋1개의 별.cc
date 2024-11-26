97a25b3995a7906bbb95c4fe94c176f50813ead6
JaeUk Lee <101004585+LeeJU0912@users.noreply.github.com>
2024-11-26 11:36:30 +0900

    [Platinum II] Title: 하늘에서 떨어지는 1, 2, ..., R-L+1개의 별, Time: 40 ms, Memory: 3588 KB -BaekjoonHub

--- 백준/Platinum/17353. 하늘에서 떨어지는 1， 2， ．．．， R－L＋1개의 별/하늘에서 떨어지는 1， 2， ．．．， R－L＋1개의 별.cc
+++ 백준/Platinum/17353. 하늘에서 떨어지는 1， 2， ．．．， R－L＋1개의 별/하늘에서 떨어지는 1， 2， ．．．， R－L＋1개의 별.cc
@@ -2,84 +2,68 @@
 #define FastIO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 using namespace std;
 
-int N;
-vector<int> A;
+int N, Q;
 vector<long long> tree;
-vector<long long> lazy;
-int Q;
+vector<int> treeL;
+vector<int> save;
 
-void propagate(int node, int left, int right) {
-    if (lazy[node] != 0) {
-        if (left != right) {
-            lazy[node * 2] += lazy[node];
-            lazy[node * 2 + 1] += lazy[node];
+void update(int target, int val) {
+    while(target <= N) {
+        tree[target] += val;
+        if (val > 0) {
+            treeL[target]++;
         }
-        tree[node] += (right - left + 1) * lazy[node];
-        lazy[node] = 0;
+        else {
+            treeL[target]--;
+        }
+        target += (target & -target);
     }
 }
 
-void update(int node, int left, int right, int start, int end, int val) {
-    propagate(node, left, right);
-    if (right < start || left > end) return;
-    if (start <= left && right <= end) {
-        lazy[node] = val;
-        propagate(node, left, right);
-        return;
+long long query(int X) {
+    long long ret = 0;
+    while(X > 0) {
+        ret += tree[X];
+        X -= (X & -X);
     }
-
-    update(node * 2, left, (left + right) / 2, start, end, val);
-    update(node * 2 + 1, (left + right) / 2 + 1, right, start, end, val);
-
-    tree[node] = tree[node * 2] + tree[node * 2 + 1];
+    return ret;
 }
 
-long long query(int node, int left, int right, int start, int end) {
-    propagate(node, left, right);
-    if (right < start || end < left) return 0;
-    if (start <= left && right <= end) {
-        return tree[node];
+long long queryL(int X) {
+    long long ret = 0;
+    while(X > 0) {
+        ret += treeL[X];
+        X -= (X & -X);
     }
-
-    long long l = query(node * 2, left, (left + right) / 2, start, end);
-    long long r = query(node * 2 + 1, (left + right) / 2 + 1, right, start, end);
-
-    return l + r;
+    return ret;
 }
 
 int main() {
     FastIO
 
     cin >> N;
-    A = vector<int>(N + 1);
-    int tree_depth = (int)ceil(log2(N));
-    int tree_size = (1 << (tree_depth + 1));
-    tree = vector<long long>(tree_size);
-    lazy = vector<long long>(tree_size);
-
-    for (int i = 1; i <= N; i++) {
-        cin >> A[i];
-    }
-
-    for (int i = 1; i <= N; i++) {
-        update(1, 1, N, i, i, A[i] - A[i - 1]);
+    tree = vector<long long>(N + 1);
+    treeL = vector<int>(N + 1);
+    save = vector<int>(N + 1);
+    for(int i = 1; i <= N; i++) {
+        cin >> save[i];
     }
 
     cin >> Q;
-    for (int i = 0; i < Q; i++) {
+    while(Q--) {
         int order;
         cin >> order;
 
         if (order == 1) {
-            int l, r;
-            cin >> l >> r;
-            update(1, 1, N, l, r, 1);
-            update(1, 1, N, r + 1, r + 1, -(r - l + 1));
+            int L, R;
+            cin >> L >> R;
+            update(L, L);
+            update(R + 1, -L);
         }
         else if (order == 2) {
             int X;
             cin >> X;
-            cout << query(1, 1, N, 1, X) << '\n';
+            cout << queryL(X) * (X + 1) - query(X) + save[X] << '\n';
         }
     }
 
