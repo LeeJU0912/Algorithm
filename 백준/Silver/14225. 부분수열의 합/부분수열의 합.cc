de02580b1620300942907202418e613887e33192
JaeUk Lee <101004585+LeeJU0912@users.noreply.github.com>
2024-11-27 11:15:19 +0900

    [Silver I] Title: 부분수열의 합, Time: 228 ms, Memory: 42420 KB -BaekjoonHub

---
+++ 백준/Silver/14225. 부분수열의 합/부분수열의 합.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define FastIO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+using namespace std;
+
+int N;
+vector<int> save;
+set<int> chk;
+
+int main() {
+    FastIO
+
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        int x;
+        cin >> x;
+        save.push_back(x);
+    }
+
+    for (int i = 0; i < N; i++) {
+        set<int> next = chk;
+        for (int x : chk) {
+            next.insert(x + save[i]);
+        }
+        next.insert(save[i]);
+        chk = next;
+    }
+
+    for (int i = 1; i <= 2000000; i++) {
+        if (chk.find(i) == chk.end()) {
+            cout << i;
+            return 0;
+        }
+    }
+
+    return 0;
+}
