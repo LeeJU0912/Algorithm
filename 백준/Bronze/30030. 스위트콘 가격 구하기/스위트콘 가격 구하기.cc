53163312ffab1f83163e0da16ad553944c891362
JaeUk Lee <101004585+LeeJU0912@users.noreply.github.com>
2024-12-07 09:53:10 +0900

    [Bronze V] Title: 스위트콘 가격 구하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30030. 스위트콘 가격 구하기/스위트콘 가격 구하기.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define FastIO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+using namespace std;
+
+int main() {
+    FastIO
+
+    int B;
+    cin >> B;
+    cout << B * 10 / 11;
+
+    return 0;
+}
