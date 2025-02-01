9d920850199d96d1b10e810e16dc8403d3597151
JaeUk Lee <101004585+LeeJU0912@users.noreply.github.com>
2025-02-01 20:07:24 +0900

    [Gold III] Title: 미로 탈출하기, Time: 8 ms, Memory: 7768 KB -BaekjoonHub

--- 백준/Gold/17090. 미로 탈출하기/미로 탈출하기.cc
+++ 백준/Gold/17090. 미로 탈출하기/미로 탈출하기.cc
@@ -5,16 +5,15 @@ using namespace std;
 int N, M;
 int board[501][501];
 int DP[501][501];
-bool visited[501][501];
 
 int dx[4] = {-1, 0, 1, 0};
 int dy[4] = {0, 1, 0, -1};
 
 int dfs(int x, int y) {
     if (x < 0 || x >= N || y < 0 || y >= M) return 1;
-    if (visited[x][y]) return -1;
     if (DP[x][y]) return DP[x][y];
-    visited[x][y] = true;
+
+    DP[x][y] = -1;
 
     int nextX = x + dx[board[x][y]];
     int nextY = y + dy[board[x][y]];
@@ -51,8 +50,6 @@ int main() {
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < M; j++) {
             if (DP[i][j]) continue;
-
-            memset(visited, false, sizeof(visited));
             dfs(i, j);
         }
     }
