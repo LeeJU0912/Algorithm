f65b541a041f39ef5bf9bf8b90856e80d2f901fd
JaeUk Lee <101004585+LeeJU0912@users.noreply.github.com>
2025-02-28 17:39:42 +0900

    [Bronze IV] Title: :chino_shock:, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27310. ：chino＿shock：/：chino＿shock：.cc
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#define FastIO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+using namespace std;
+
+int main() {
+    FastIO
+
+    string s;
+    cin >> s;
+
+    int ans = 2 + s.size();
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == '_') ans += 5;
+    }
+
+    cout << ans;
+
+    return 0;
+}
