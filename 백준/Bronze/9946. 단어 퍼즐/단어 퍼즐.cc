c722a6264e6f993eb352f4cd757603f38ca8fef1
JaeUk Lee <101004585+LeeJU0912@users.noreply.github.com>
2025-04-12 14:52:06 +0900

    [Bronze I] Title: 단어 퍼즐, Time: 136 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/9946. 단어 퍼즐/단어 퍼즐.cc
+++ 백준/Bronze/9946. 단어 퍼즐/단어 퍼즐.cc
@@ -2,45 +2,6 @@
 #define FastIO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 using namespace std;
 
-void merge(string& s, int left, int mid, int right) {
-    vector<char> sorted = vector<char>(s.size());
-
-    int cursor = left;
-    int l = left;
-    int r = mid + 1;
-
-    while(l <= mid && r <= right) {
-        if (s[l] <= s[r]) {
-            sorted[cursor++] = s[l++];
-        }
-        else {
-            sorted[cursor++] = s[r++];
-        }
-    }
-
-    for (int i = l; i <= mid; i++) {
-        sorted[cursor++] = s[i];
-    }
-    for (int i = r; i <= right; i++) {
-        sorted[cursor++] = s[i];
-    }
-
-    for (int i = left; i <= right; i++) {
-        s[i] = sorted[i];
-    }
-}
-
-void mergeSort(string& s, int left, int right) {
-    if (left >= right) return;
-
-    int mid = (left + right) / 2;
-
-    mergeSort(s, left, mid);
-    mergeSort(s, mid + 1, right);
-
-    merge(s, left, mid, right);
-}
-
 int main() {
     FastIO
 
@@ -52,8 +13,8 @@ int main() {
 
         if (a == "END" && b == "END") break;
 
-        mergeSort(a, 0, a.size() - 1);
-        mergeSort(b, 0, b.size() - 1);
+        sort(a.begin(), a.end());
+        sort(b.begin(), b.end());
 
         if (a == b) {
             cout << "Case " << t++ << ": " << "same" << '\n';
