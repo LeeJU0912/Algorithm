b2021b8443dc66e409187304d2c8e2006cb417b7
JaeUk Lee <101004585+LeeJU0912@users.noreply.github.com>
2025-05-11 19:34:44 +0900

    [Silver I] Title: 관중석, Time: 244 ms, Memory: 18396 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/10166. 관중석/관중석.java
+++ 백준/Silver/10166. 관중석/관중석.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.math.BigInteger;
 import java.util.*;
 import java.util.stream.IntStream;
 
@@ -6,23 +7,35 @@ import static java.lang.Math.*;
 
 public class Main {
 
+    static int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        Set<Integer> set = new HashSet<>();
+        boolean[][] chk = new boolean[2001][2001];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         int D1 = Integer.parseInt(st.nextToken());
         int D2 = Integer.parseInt(st.nextToken());
 
+        int ans = 0;
         for (int i = D1; i <= D2; i++) {
             for (int j = 1; j <= i; j++) {
-                set.add(720000 * j / i);
+                int gcd = gcd(i, j);
+
+                if (chk[j / gcd][i / gcd]) {
+                    continue;
+                }
+                chk[j / gcd][i / gcd] = true;
+                ans++;
             }
         }
 
-        bw.write(String.valueOf(set.size()));
+        bw.write(String.valueOf(ans));
 
         bw.flush();
         bw.close();
